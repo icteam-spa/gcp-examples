40595ed43346cd620aa284d84572248e626d5655
Michele Milesi <michele.milesi@icteam.it>
2019-06-27 07:29:59 +0200

    Added __init__ to Cube class

--- rubik/functions/rubik/rubik.py
+++ rubik/functions/rubik/rubik.py
@@ -1,20 +1,23 @@
-class Cube:
-    NAC = ""
-    BLUE = "B"
-    RED = "R"
-    GREEN = "G"
-    YELLOW = "Y"
-    ORANGE = "O"
-    WHITE = "W"
+NAC = ""
+BLUE = "B"
+RED = "R"
+GREEN = "G"
+YELLOW = "Y"
+ORANGE = "O"
+WHITE = "W"
 
-    faces = [
-        [NAC, NAC, NAC, NAC, BLUE, NAC, NAC, NAC, NAC],
-        [NAC, NAC, NAC, NAC, RED, NAC, NAC, NAC, NAC],
-        [NAC, NAC, NAC, NAC, GREEN, NAC, NAC, NAC, NAC],
-        [NAC, NAC, NAC, NAC, YELLOW, NAC, NAC, NAC, NAC],
-        [NAC, NAC, NAC, NAC, ORANGE, NAC, NAC, NAC, NAC],
-        [NAC, NAC, NAC, NAC, WHITE, NAC, NAC, NAC, NAC]
-    ]
+
+class Cube:
+
+    def __init__(self):
+        self.faces = [
+            [NAC, NAC, NAC, NAC, BLUE, NAC, NAC, NAC, NAC],
+            [NAC, NAC, NAC, NAC, RED, NAC, NAC, NAC, NAC],
+            [NAC, NAC, NAC, NAC, GREEN, NAC, NAC, NAC, NAC],
+            [NAC, NAC, NAC, NAC, YELLOW, NAC, NAC, NAC, NAC],
+            [NAC, NAC, NAC, NAC, ORANGE, NAC, NAC, NAC, NAC],
+            [NAC, NAC, NAC, NAC, WHITE, NAC, NAC, NAC, NAC]
+        ]
 
 
 def random_cube():
