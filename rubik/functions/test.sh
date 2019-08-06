8d1d466f970d4cd5384753384c1a7cd625b7739c
Michele Milesi <michele.milesi@icteam.it>
2019-08-06 18:24:35 +0200

    Added authentication to function request

--- rubik/functions/test.sh
+++ rubik/functions/test.sh
@@ -5,9 +5,12 @@ base_url=$2
 
 case ${target} in
     rubik)
-            cube=$(curl ${base_url}/rubik_random)
+            cube=$(curl ${base_url}/rubik_random -H "Authorization: bearer $(gcloud auth print-identity-token)")
             echo '{"cube":' > request.txt
-            echo $cube >> request.txt
+            echo ${cube} >> request.txt
             echo '}' >> request.txt
+            valid=$(curl ${base_url}/rubik_validate --data @request.txt -H "Content-Type: application/json" -H "Authorization: bearer $(gcloud auth print-identity-token)")
+            rm request.txt
+            echo ${valid} | grep -c '{"valid": true}'
     ;;
 esac
